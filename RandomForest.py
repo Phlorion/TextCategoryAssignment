22ea503b54765b7a2038feb4d18a06733ccaeeef
Gaspar <rigos.gasparis@gmail.com>
2023-12-24 15:38:36 +0200

    random forest changes

--- RandomForest.py
+++ RandomForest.py
@@ -1,16 +1,17 @@
 from id3  import *
-from preprocess import *
 from imdbDataSet import *
 import numpy as np
 import keras as kr
-# Ορισμός υπερπαραμέτρων
+
+#Ορισμός υπερπαραμέτρων
 tree_number = 10
 fv_skip_top = 200
 fv_num_words = 5000 
 
-#imdb
+#obtain imdb data 
 imdb = IMDB()
 imdb.getTrainingData(skip_top=fv_skip_top, num_words=fv_num_words)
 
+#create encoded feature vector (index of words)
 feature_vector = imdb.getFeatureVector(skip_top=fv_skip_top, num_words=fv_num_words)
 
